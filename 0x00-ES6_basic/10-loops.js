2783116a09b3a911b1f3fb1bf5061069ae44d19c
Peter Omollo <omollpeter@gmail.com>
2024-07-23 01:54:45 +0300

    Use for..of to iterate over array

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [index, value] of array.entries()) {
+  for (let value of array) {
+    const index = array.indexOf(value);
     value = appendString + value;
     array[index] = value;
   }
