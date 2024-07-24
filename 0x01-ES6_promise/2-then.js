80e348b5de192904c43f8c5a77f871eebcdf518c
Peter Omollo <omollpeter@gmail.com>
2024-07-25 02:36:54 +0300

    Write a function that logs to the console if a promise is fulfilled

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({ status: 200, body: "Success" });
-      console.log("Got a response from the API");
-    } else {
-      reject(Error());
-    }
-  });
+  return promise
+    .then(function () {
+        console.log("Got a response from the API");
+        return { status: 200, body: 'Success' };
+    })
+    .catch((error) => {
+        return Error();
+    })
 }
