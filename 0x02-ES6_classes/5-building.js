95556bb019152167b4530c1a40a83dc641eaea6d
Peter Omollo <omollpeter@gmail.com>
2024-07-26 05:28:34 +0300

    Implement a class that is considered as an abstaract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== "number") {
+      throw TypeError("Sqft must be a number");
+    }
+    if (this.evacuationWarningMessage === undefined) {
+      throw Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== "number") {
+      throw TypeError("Sqft must be a number");
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
