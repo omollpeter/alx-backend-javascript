f15608b8cfdde195404619fc11f0705cc6b72c77
Peter Omollo <omollpeter@gmail.com>
2024-07-26 03:09:34 +0300

    Implement a Currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(code, name) {
+    if (typeof code !== "string") {
+      throw TypeError("Code must be a string");
+    }
+
+    if (typeof name !== "string") {
+      throw TypeError("Name must be a string");
+    }
+
+    this._name = name;
+    this._code = code;
+  }
+
+  set code(code) {
+    if (typeof code !== "string") {
+      throw TypeError("Code must be a string");
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== "string") {
+      throw TypeError("Name must be a string");
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
