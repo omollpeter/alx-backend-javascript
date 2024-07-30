a95f86a0f8659e9bdfd20a3ab686ea64ce6931ea
Peter Omollo <omollpeter@gmail.com>
2024-07-30 04:58:29 +0300

    Create a function that maps grade to the student

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+export default function updateStudentGradeByCity(listOfStudents, city, grades) {
+  const studGrade = listOfStudents.filter((stud) => stud.location === city).map((student) => {
+    for (let i = 0; i < grades.length; i++) {
+      if (grades[i].studentId === student.id) {
+        student.grade = grades[i].grade;
+        break;
+      }
+      student.grade = 'N/A';
+    }
+    return student;
+  });
+
+  return studGrade;
+}
