a78b259bd8f4e5bbffbdd8dc3cad56ec9c1feff9
Peter Omollo <omollpeter@gmail.com>
2024-07-30 04:01:44 +0300

    Fix 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,7 @@
 export default function getStudentsByLocation(studentList, location) {
+  if (!(studentList instanceof Array)) {
+    return [];
+  }
   const studentsInLocation = studentList.filter((student) => student.location === location);
 
   return studentsInLocation;
