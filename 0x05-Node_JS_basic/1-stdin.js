9185c23c6c0f77aa07110c7134fe5ac27d2254b5
Peter Omollo <omollpeter@gmail.com>
2024-09-17 08:41:47 +0300

    Fix 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,17 @@
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-console.log('Welcome to Holberton School, what is your name?');
+console.log("Welcome to Holberton School, what is your name?");
 
 if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    console.log('Your name is:', data.trim());
-    // process.exit();
+  process.stdin.on("data", (data) => {
+    console.log("Your name is:", data.trim());
   });
 } else {
-  process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-
-    if (chunk) {
-      console.log('Your name is:', chunk.trim());
-    }
+  process.stdin.on("data", (data) => {
+    console.log("Your name is:", data.trim());
   });
 
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  process.stdin.on("end", () => {
+    console.log("This important software is now closing");
   });
 }
