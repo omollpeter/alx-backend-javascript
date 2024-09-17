1723288f097e19a71878d0c8a92ce710c80ba0a1
Peter Omollo <omollpeter@gmail.com>
2024-09-17 08:27:15 +0300

    Create a program that reads input from a user

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,13 +2,21 @@ process.stdin.setEncoding('utf-8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk) {
-    console.log('Your name is:', chunk.trim());
-  }
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    console.log('Your name is:', data.trim());
+    process.exit();
+  });
+} else {
+  process.stdin.on('readable', () => {
+    const chunk = process.stdin.read();
 
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
-});
+    if (chunk) {
+      console.log('Your name is:', chunk.trim());
+    }
+  });
+
+  process.stdin.on('end', () => {
+    console.log('The important software is now closing');
+  });
+}
