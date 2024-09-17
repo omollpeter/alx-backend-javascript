4fa1f25828d213c910526516c563ab4501c3e025
Peter Omollo <omollpeter@gmail.com>
2024-09-17 19:11:13 +0300

    Create a more complex server with express js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const dbPath = process.argv[2];
+  countStudents(dbPath).then((result) => {
+    res.send(`This is the list of our students\n${result}`);
+  });
+});
+
+app.listen(1245);
+
+module.exports = app;
