1adbd0dd857e8659c1334c899a70ac9c94d4beeb
Peter Omollo <omollpeter@gmail.com>
2024-09-19 18:09:18 +0300

    Write basic integration tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,11 +1,24 @@
 const app = require("./api");
-const request = require("supertest");
+// const request = require("supertest");
+
+// describe("Integration Test for API", () => {
+//   it("Should return 200 OK with correct message", (done) => {
+//     request(app)
+//       .get("/")
+//       .expect(200)
+//       .expect("Welcome to the payment system", done);
+//   });
+// });
+
+const request = require("request");
+const assert = require("assert");
 
 describe("Integration Test for API", () => {
   it("Should return 200 OK with correct message", (done) => {
-    request(app)
-      .get("/")
-      .expect(200)
-      .expect("Welcome to the payment system", done);
+    request.get("http://localhost:7865/", (error, response, body) => {
+      assert.strictEqual(response.statusCode, 200);
+      assert.strictEqual(body, "Welcome to the payment system");
+      done();
+    });
   });
 });
